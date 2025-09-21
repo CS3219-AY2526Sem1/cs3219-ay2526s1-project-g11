b9750df56382c31258199698af18d3bfebf76cd7
Yiyang <liuyiyang0929@gmail.com>
2025-09-22 00:58:42 +0800

    feat: add chat to session page

--- frontend/src/components/CustomPopover.tsx
+++ frontend/src/components/CustomPopover.tsx
@@ -20,7 +20,7 @@ export const CustomPopover = ({
       <Popover.Portal>
         <Popover.Content
           align={align}
-          className="min-w-12 min-h-8 w-fit h-fit bg-white/30 backdrop-blur-sm shadow-sm rounded-lg mt-2"
+          className="min-w-12 min-h-8 w-fit h-fit bg-white shadow-sm rounded-lg mt-2 z-10"
         >
           {children}
         </Popover.Content>
