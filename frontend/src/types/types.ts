58cdeb4de505a3a9037bd8daca098f453df9aa10
0nesheep <liuyiyang0929@gmail.com>
2025-09-27 17:08:04 +0800

    fix: update session editor to be uncontrolled for consistency

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,17 +1,35 @@
 export interface User {
-  id: string;
-  username: string;
-  email: string;
-  isAdmin: boolean;
-  createdAt: string;
+	id: string;
+	username: string;
+	email: string;
+	isAdmin: boolean;
+	createdAt: string;
 }
 
 export interface VerifyTokenResponse {
-  message: string;
-  data: User;
+	message: string;
+	data: User;
 }
 
 export interface LoginResponse {
-  message: string;
-  data: User & { accessToken: string };
+	message: string;
+	data: User & { accessToken: string };
 }
+
+export type SessionJoinResponse = {
+	user_id: string;
+	rev: number;
+	text: string;
+};
+
+export type Delta = {
+	from: number;
+	to: number;
+	text: string;
+};
+
+export type CodeUpdateResponse = {
+	rev: number;
+	delta: Delta;
+	by: string;
+};
