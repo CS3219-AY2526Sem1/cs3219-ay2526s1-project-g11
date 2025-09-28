986499e3644fe0a925317f556ca619a2a936f3ed
owenyeo <owen_nly@hotmail.com>
2025-09-28 20:35:39 +0800

    Edit runtime.exs

--- backend/collab-service/config/config.exs
+++ backend/collab-service/config/config.exs
@@ -1,4 +1,10 @@
-# config/runtime.exs
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
 
 if config_env() == :prod do
@@ -10,7 +16,7 @@ if config_env() == :prod do
     You can generate one by calling: mix phx.gen.secret
     """
 
-  host = System.get_env("PUBLIC_URL") || "your-service-name.run.app"
+  host = System.get_env("PUBLIC_URL") || "collab-service-1015946686380.europe-west1.run.app"
   port = String.to_integer(System.get_env("PORT", "8080"))
 
   config :collab_service, CollabServiceWeb.Endpoint,
@@ -23,3 +29,45 @@ if config_env() == :prod do
     secret_key_base: secret_key_base,
     server: true
 end
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :collab_service, CollabService.Mailer, adapter: Swoosh.Adapters.Local
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.25.4",
+  collab_service: [
+    args:
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "4.1.7",
+  collab_service: [
+    args: ~w(
+      --input=assets/css/app.css
+      --output=priv/static/assets/css/app.css
+    ),
+    cd: Path.expand("..", __DIR__)
+  ]
+
+# Configures Elixir's Logger
+config :logger, :default_formatter,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
