1a7c7cb92ee3857b04b59d94ab94a965183c3e11
owenyeo <owen_nly@hotmail.com>
2025-09-28 19:48:47 +0800

    Fix config.exs

--- backend/collab-service/config/config.exs
+++ backend/collab-service/config/config.exs
@@ -10,25 +10,16 @@ import Config
 config :collab_service,
   generators: [timestamp_type: :utc_datetime]
 
-allowed =
-  [
-    "https://YOUR_RUN_HOST.a.run.app",
-    "https://your-frontend.example.com"
-  ]
-
 config :collab_service, CollabServiceWeb.Endpoint,
-  check_origin: allowed
-
-# Configures the endpoint
-# config :collab_service, CollabServiceWeb.Endpoint,
-#   url: [host: "localhost"],
-#   adapter: Bandit.PhoenixAdapter,
-#   render_errors: [
-#     formats: [html: CollabServiceWeb.ErrorHTML, json: CollabServiceWeb.ErrorJSON],
-#     layout: false
-#   ],
-#   pubsub_server: CollabService.PubSub,
-#   live_view: [signing_salt: "1o83+uil"]
+  url: [host: "collab-service-1015946686380.europe-west1.run.app", port: 443, scheme: "https"],
+  check_origin: ["https://collab-service-1015946686380.europe-west1.run.app", "https://your-frontend.com"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: CollabServiceWeb.ErrorHTML, json: CollabServiceWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: CollabService.PubSub,
+  live_view: [signing_salt: "1o83+uil"]
 
 # Configures the mailer
 #
