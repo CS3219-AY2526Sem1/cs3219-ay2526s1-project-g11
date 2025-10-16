a82b373e88736dc5124547fd33dd84db33a8ed10
EricXiong420 <E0958551@u.nus.edu>
2025-10-17 01:37:55 +0800

    Added CRUD operations for questions. Added getTopics endpoint. Added gcloud deploy script. Added API key authentication for endpoints.

--- backend/question-service/src/index.js
+++ backend/question-service/src/index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 8080;
 
 app.use('/questions', require('./routes/questionRoutes'));
+app.use('/topics', require('./routes/topicRoutes'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
