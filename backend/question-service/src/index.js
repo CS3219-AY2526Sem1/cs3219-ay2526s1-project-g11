4e387f67b33715bd337ed0c411dd7a234ebe7842
Cheng-Yu Dong <39908329+officialchengyud@users.noreply.github.com>
2025-11-12 01:32:13 +0800

    fix(question-service): cors

--- backend/question-service/src/index.js
+++ backend/question-service/src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const { connectDB } = require('./db');
 connectDB();
 
 const app = express();
+
+const corsOptions = {
+  origin: ["https://cs3219-ay2526s1-project-g11.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
