e5d54a96c0025551d22e190b0b22f39427f687fe
0nesheep <liuyiyang0929@gmail.com>
2025-11-12 01:59:43 +0800

    fix: fix question service api calls

--- frontend/src/api/QuestionService.ts
+++ frontend/src/api/QuestionService.ts
@@ -1,35 +1,45 @@
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 import type { GetQuestionByIdResponse } from "../types/types";
 
-// const QUESTION_API_BASE_URL = import.meta.env.VITE_QUESTION_API_BASE_URL;
+const QUESTION_API_BASE_URL = import.meta.env.VITE_QUESTION_API_BASE_URL;
 
-// const apiClient = axios.create({
-//   baseURL: QUESTION_API_BASE_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+const apiClient = axios.create({
+  baseURL: QUESTION_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-// const questionServiceAPIRequest = async <T>(
-//   url: string,
-//   method: "GET" | "POST" | "PUT" | "DELETE",
-//   data?: unknown,
-//   headers?: Record<string, string>
-// ): Promise<T> => {
-//   const response: AxiosResponse<T> = await apiClient({
-//     method,
-//     url,
-//     data,
-//     headers,
-//   });
+const questionServiceAPIRequest = async <T>(
+  url: string,
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  data?: unknown,
+  headers?: Record<string, string>,
+): Promise<T> => {
+  const response: AxiosResponse<T> = await apiClient({
+    method,
+    url,
+    data,
+    headers,
+  });
 
-//   return response.data;
-// };
+  return response.data;
+};
 
-// bypass CORS in dev for testing
 export const getQuestionById = async (
   id: string,
 ): Promise<GetQuestionByIdResponse> => {
-  const response = await axios.get(`/questions/${id}`);
-  return response.data;
+  const response = await questionServiceAPIRequest<GetQuestionByIdResponse>(
+    `/questions/${id}`,
+    "GET",
+  );
+  return response;
 };
+
+// bypass CORS in dev for testing
+// export const getQuestionById = async (
+//   id: string,
+// ): Promise<GetQuestionByIdResponse> => {
+//   const response = await axios.get(`/questions/${id}`);
+//   return response.data;
+// };
